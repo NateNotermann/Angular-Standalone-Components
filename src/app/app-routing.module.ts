4f597764c31f42c66eaf6ecdda663bc57b3c7f86
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-04-28 19:54:59 -0500

    done!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModel } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { DogViewComponent } from './dog-view.component';
+// import { DogViewComponent } from './dog-view.component'; // Removing STATIC IMPORT
 import { DogslistComponent } from './dogslist.component';
 import { NgModule } from '@angular/core';
 // import { DogsListCardComponent } from './dogs-list-card.component'; // Doesn't need to be here. b/c its standalone
@@ -9,7 +9,11 @@ const routes: Routes = [
     {path:'', pathMatch: 'full', redirectTo: 'list' },
     {path: 'list', component: DogslistComponent },
     // {path: 'list-card', component: DogsListCardComponent },  // Doesn't need to be here. b/c its standalone
-    {path: 'details/:index', component: DogViewComponent},
+
+    // adding DYNAMIC IMPORT below
+    // This is "LAZY LOADING". It only loads when the ROUTE is hit.
+    // This means the DogView is not part of the main/initial bundle. Improving app performance.
+    {path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m=> m.DogViewComponent) },
 ];
 
 @NgModule({
